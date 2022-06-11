c84868cc12c58087320e79e104a8d009cb1c7e97
maayaankk <mayankchotaliya2001@gmail.com>
2022-06-11 23:33:25 +0530

    basic code in strings

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+string myName = "Mayank";
+string myLastName = "Chotaliya";
+
+//Basic console.writeline
+Console.WriteLine(myName);
+Console.WriteLine(myLastName);
+
+//String interpotation
+Console.WriteLine("Hello " + myName);
+Console.WriteLine($"Hello {myName}");
+
+Console.WriteLine($"My Name is {myName} and my last name is {myLastName}");
+
+//Finding length in the string
+Console.WriteLine($"My name {myName} has {myName.Length} Characters");
+
+
+
